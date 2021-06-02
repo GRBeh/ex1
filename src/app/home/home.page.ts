f60e58e46c4d2d7cc07ca4286b7a10d60d664a79
beh <behguanrong@gmail.com>
2021-06-02 16:57:19 +0800

    looped

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Container } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 import { DataService } from "../service/data.service"
 
 @Component({
@@ -10,23 +11,28 @@ import { DataService } from "../service/data.service"
 export class HomePage implements OnInit {
   constructor(private dataService: DataService) { }
   ngOnInit() {
-    this.dataService.getAll().subscribe(data2 =>{
-      console.log(data2);   
+    this.dataService.getAll().subscribe(data2 => {
+      let dt = "";
       data2.results.forEach(element => {
-        console.log(element.url);
-        console.log(element.name);
+        dt = dt + '<ion-card>'
+          + '<p id = "name-' + element.name + '">filler</p>'
+          + '<p id = "sprite-' + element.name + '">filler</p>'
+          + '<p id = "moves-' + element.name + '">filler</p>'
+          + '</ion-card>'
+      });
+      document.getElementById("pokemon").innerHTML = dt;
+      data2.results.forEach(element => {
         this.dataService.getRemoteData(element.url).subscribe(data => {
-          console.log(data);
-          document.getElementById("name").innerHTML =
+          document.getElementById("name-" + data.name + "").innerHTML =
             '<ion-card-header><ion-card-title class="ion-text-capitalize">' + data.name + '</ion-card-title></ion-card-header>';
           let moveList = "";
           data.moves.forEach(move => {
             moveList = moveList + "<ion-card-content>" + move.move.name + "</ion-card-content>"
           });
-          document.getElementById("moves").innerHTML = moveList;
-          document.getElementById("sprite").innerHTML = "<img src=\"" + data.sprites.front_default + "\">";
+          document.getElementById("moves-" + data.name + "").innerHTML = moveList;
+          document.getElementById("sprite-" + data.name + "").innerHTML = "<img src=\"" + data.sprites.front_default + "\">";
         });
-      });    
+      });
     });
   }
 }
