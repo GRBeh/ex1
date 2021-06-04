10dcf8244f3723d6fd51d93d996b10b7edf7ceb3
beh <behguanrong@gmail.com>
2021-06-04 13:08:52 +0800

    6/4 1pm

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,8 @@
 import { Container } from '@angular/compiler/src/i18n/i18n_ast';
+import { getAllLifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
 import { Component, OnInit } from '@angular/core';
-import { element } from 'protractor';
-import { DataService } from "../service/data.service"
+import { resolve } from 'dns';
+import { element } from 'protractor'; 
 
 @Component({
   selector: 'app-home',
@@ -9,29 +10,33 @@ import { DataService } from "../service/data.service"
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  detailsList =[]
 
-  constructor(private dataService: DataService) {
-    this.getAll();
-   }
-  ngOnInit() { }
+  detailsList: any
 
-  getAll(){
-    let promise = new Promise((resolve,reject)=>{
-      this.dataService.getAll().subscribe( item => { 
-        resolve(item)
-      })
-    });   
-    promise.then(res => this.getById(res));
+  constructor() {
+    //this.getAll();
   }
-  
-
-  getById(item){
-    item.results.forEach(pokemon => {
-        this.dataService.getById(pokemon.url).subscribe(details =>{
-        this.detailsList.push(details);
-        });
-    }); 
+  ngOnInit() {
+  this.fetchPokemon();
   }
-  
-}
+
+  fetchPokemon(){
+    const promises = [];
+    let promise;
+    let idMax = 15; 
+     for (let id = 1; id <= idMax; id++) {
+       const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+       promises.push(fetch(url).then((res) => res.json()));
+     }
+    Promise.all(promises).then((results) => {
+      const pokemon = results.map((result) => ({
+        name: result.name,
+        images: result.sprites['front_default'],
+        moves: result.moves,
+        type: result.types.map((type) => type.type.name).join(', '),
+        id: result.id
+      }));
+      this.detailsList = pokemon;
+    });
+  };
+ }
