842679eda4a767161162eab8b88c442fdb883c44
beh <behguanrong@gmail.com>
2021-06-08 10:52:16 +0800

    6/8

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,9 @@
 import { Container } from '@angular/compiler/src/i18n/i18n_ast';
 import { getAllLifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { resolve } from 'dns';
 import { element } from 'protractor'; 
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +13,25 @@ import { element } from 'protractor';
 export class HomePage implements OnInit {
 
   detailsList: any
+  
 
-  constructor() {
-    //this.getAll();
+  constructor(private socialSharing: SocialSharing) { 
   }
+  
   ngOnInit() {
   this.fetchPokemon();
   }
+
+  printSkillName(value){
+    console.log(value);
+
+    this.socialSharing.canShareViaEmail().then(() => {
+      console.log("yes");
+    }).catch(() => {
+      console.log("no");
+    });
+  }
+
 
   fetchPokemon(){
     const promises = [];
@@ -32,11 +45,12 @@ export class HomePage implements OnInit {
       const pokemon = results.map((result) => ({
         name: result.name,
         images: result.sprites['front_default'],
-        moves: result.moves,
+        moves: result.moves.map((moves) => moves.move.name),
         type: result.types.map((type) => type.type.name).join(', '),
         id: result.id
       }));
       this.detailsList = pokemon;
+      console.log(this.detailsList)
     });
   };
  }
