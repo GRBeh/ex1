7da8f243b18a7bfabbfce286c4fb63088a2f1aa9
beh <behguanrong@gmail.com>
2021-06-09 13:50:01 +0800

    6/9

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
+import { ViewChild } from '@angular/core';
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +9,18 @@ import { SocialSharing } from '@ionic-native/social-sharing/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  @ViewChild(IonContent) content: IonContent;
 
   detailsList: any 
+  idMin = 1
+  value:number = 1
+  numPerPage = 1;
 
   constructor(private socialSharing:SocialSharing){
   }
 
   ngOnInit() {
-    this.fetchPokemon();
+    this.fetchPokemon(this.idMin);
   }
 
   shareWhatsapp(value) {
@@ -27,11 +33,15 @@ export class HomePage implements OnInit {
     this.socialSharing.shareViaTwitter(value, null, null);
   }
 
-  fetchPokemon() {
+  fetchPokemon(idMin:number) {
     const promises = [];
     let promise;
-    let idMax = 15;
-    for (let id = 1; id <= idMax; id++) {
+    
+    if(idMin<1)idMin = 1;
+    let idMax = idMin + this.numPerPage;
+    
+
+    for (let id = idMin; id < idMax; id++) {
       const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
       promises.push(fetch(url).then((res) => res.json()));
     }
@@ -47,4 +57,33 @@ export class HomePage implements OnInit {
       console.log(this.detailsList)
     });
   };
+
+  nextPage(){
+    console.log("nextpage");
+   
+    if((this.idMin+this.numPerPage )<600){
+      this.idMin = this.idMin + this.numPerPage;
+      this.fetchPokemon(this.idMin);      
+      this.content.scrollToTop(1000);
+    console.log("+",this.idMin);
+    }
+  }
+
+  previousPage(){
+    console.log("previouspage");
+    if((this.idMin-this.numPerPage )>1){
+    this.idMin = this.idMin - this.numPerPage;
+    this.fetchPokemon(this.idMin);
+    this.content.scrollToTop(1000);
+    console.log("-",this.idMin);
+    }
+  }
+
+  setIdMin(){
+    if(this.value>0 && this.value != this.idMin){
+      this.idMin = this.value;
+      this.fetchPokemon(this.idMin);
+      this.content.scrollToTop(1000);
+    }
+  }
 }
